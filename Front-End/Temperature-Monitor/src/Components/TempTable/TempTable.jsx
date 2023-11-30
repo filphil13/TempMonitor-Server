f2497879d3685205dfb6c2db1afb7aed5f6a25f0
filphil12 <filphil13@gmail.com>
2023-11-29 21:53:53 -0500

    changed url to http rather than https

--- Front-End/Temperature-Monitor/src/Components/TempTable/TempTable.jsx
+++ Front-End/Temperature-Monitor/src/Components/TempTable/TempTable.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {useState, useEffect} from 'react';
 
-const API_URL = "https://temp-monitor-a38f32c02c5e.herokuapp.com"
+const API_URL = "http://temp-monitor-a38f32c02c5e.herokuapp.com"
 
 
 
