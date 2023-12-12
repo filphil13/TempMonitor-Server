b61f1c92f31ed63723956ca0e9edccdc38c9800e
filphil12 <filphil13@gmail.com>
2023-12-11 23:32:19 -0500

    Update script and logo sources

--- Front-End/Temperature-Monitor/src/Components/Navbar/Navbar.jsx
+++ Front-End/Temperature-Monitor/src/Components/Navbar/Navbar.jsx
@@ -16,7 +16,7 @@ function Navbar() {
 		<nav class="bg-white border-gray-200 dark:bg-gray-900">
 			<div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
 				<a href="#" class="flex items-center">
-					<img src="src/assets/OIP.png" class="h-8 w-full mr-3" alt="Temperature Monitor Logo" />
+					<img src="https://flowbite.com/docs/images/logo.svg" class="h-8 mr-3" alt="Temperature Monitor Logo" />
 					<span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Temperature Monitor</span>
 				</a>	
 				<div class="flex md:order-2">
